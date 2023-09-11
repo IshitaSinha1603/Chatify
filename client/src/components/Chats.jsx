9eb9924d20d141cc2e411d5880ad21b5d9e8e80d
ishita <ishitasinha16@gmail.com>
2023-09-12 00:26:43 +0530

    Adding chats in the sidebar

---
+++ client/src/components/Chats.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Chats = () => {
+    return (
+        <div className="chats">
+            <div className="userChat">
+                <img
+                    src="https://images.pexels.com/photos/18201340/pexels-photo-18201340/free-photo-of-redhead-woman-looking-up.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <div className="userChatInfo">
+                        <span>Jane</span>
+                        <p>Hello</p>
+                    </div>
+            </div>
+            <div className="userChat">
+                <img
+                    src="https://images.pexels.com/photos/18201340/pexels-photo-18201340/free-photo-of-redhead-woman-looking-up.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <div className="userChatInfo">
+                        <span>Jane</span>
+                        <p>Hello</p>
+                    </div>
+            </div>
+            <div className="userChat">
+                <img
+                    src="https://images.pexels.com/photos/18201340/pexels-photo-18201340/free-photo-of-redhead-woman-looking-up.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <div className="userChatInfo">
+                        <span>Jane</span>
+                        <p>Hello</p>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Chats;
