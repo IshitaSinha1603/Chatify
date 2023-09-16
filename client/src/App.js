5df6f370d40a9e1e0d4ee687c1004e870991d8f4
ishita <ishitasinha16@gmail.com>
2023-09-16 17:22:56 +0530

    Adding login authentication

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,30 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import "../src/pages/style.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser) {
+      return <Navigate to="/login" />
+    }
+    return children;
+  }
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+        <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Navbar = () => {
     return (
@@ -7,7 +9,7 @@ const Navbar = () => {
             <div className="user">
                 <img src="https://images.pexels.com/photos/18201340/pexels-photo-18201340/free-photo-of-redhead-woman-looking-up.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 <span>John</span>
-                <button>Logout</button>
+                <button onClick={()=>signOut(auth)}>Logout</button>
             </div>
         </div>
     )
