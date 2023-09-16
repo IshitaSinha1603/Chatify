32b35e1646716ef912814f7d1efdfc4535241344
ishita <ishitasinha16@gmail.com>
2023-09-16 16:47:27 +0530

    Adding register authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,19 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import "../src/pages/style.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
