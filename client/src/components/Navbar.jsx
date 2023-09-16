2f4d14b47770412bcc42ede3e51209b431a516d7
ishita <ishitasinha16@gmail.com>
2023-09-16 17:28:25 +0530

    Fetching user from firebase

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+    const {currentUser} = useContext(AuthContext);
     return (
         <div className="navbar">
             <span className="logo">Chatify</span>
             <div className="user">
-                <img src="https://images.pexels.com/photos/18201340/pexels-photo-18201340/free-photo-of-redhead-woman-looking-up.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <span>John</span>
+                <img src={currentUser.photoURL} alt="" />
+                <span>{currentUser.displayName}</span>
                 <button onClick={()=>signOut(auth)}>Logout</button>
             </div>
         </div>
